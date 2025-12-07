33a57fd8fb300d13fe34347d8662dacce4886a57
PodushkaPIR <seterovas@yandex.ru>
2025-12-07 12:06:15 +0300

    Add new packages

--- home-manager/home-packages.nix
+++ home-manager/home-packages.nix
@@ -3,16 +3,25 @@
 
   home.packages = with pkgs; [
     # Desktop apps
+    amnezia-vpn
+    anki
+    brave
     mpv
     telegram-desktop
     obsidian
-    amnezia-vpn
+    pavucontrol
     zathura
     discord
+    wireshark
 
     # CLI utils
+    autoconf
+    tree
     cliphist
+    cmake
     ffmpeg
+    ffmpegthumbnailer
+    fzf
     nsxiv
     htop
     hyprpicker
@@ -22,21 +31,30 @@
     wget
     yt-dlp
     neovim
+    playerctl
+    pkg-config
+    neofetch
     grimblast
     wl-clipboard
-
+    yazi
 
     # Coding stuff
     nodejs
+    python312Packages.pip
+    python312Packages.ninja
+    boost
+    go
 
     # WM
     xdg-desktop-portal-gtk
     xdg-desktop-portal-hyprland
     
     # Other
+    rocmPackages.llvm.clang-unwrapped
+    amneziawg-tools
+    wireguard-tools
     brightnessctl
     iptables
     iproute2
-    wireguard-tools
   ];
 }
